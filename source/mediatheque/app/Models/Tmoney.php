ae00bf67e88eadcd794e0ae8f17a7d1a9fcc34cb
Alhassan-Daiki <Alhassan.blog@gmail.com>
2022-08-16 16:53:05 +0000

    Model Laravel

---
+++ source/mediatheque/app/Models/Tmoney.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tmoney extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id_registraton'];
+    protected $primaryKey = 'id_tmoney';
+
+
+    public function registration()
+    {
+        return $this->belongsTo(Registration::class);
+    }
+}
