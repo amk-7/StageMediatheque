a00170f5091b234f124e8bba52daf1d12e5884d6
Alhassan-Daiki <Alhassan.blog@gmail.com>
2022-08-17 11:54:43 +0000

    Routes

--- source/mediatheque/routes/web.php
+++ source/mediatheque/routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+
+
+
+
+
+// Path: Abonne routes/web.php
+Route::get('liste_des_abonnes', 'App\Http\Controllers\AbonneController@index')->name('listeAbonnes');
+Route::get('affiche_abonne', 'App\Http\Controllers\AbonneController@show')->name('showAbonne');
+Route::get('formulaire_edition_des_abonnes', 'App\Http\Controllers\AbonneController@edit')->name('editAbonne');
+Route::put('mise_a_jour_des_abonnes', 'App\Http\Controllers\AbonneController@update')->name('updateAbonne');
+Route::delete('suppression_des_abonnes', 'App\Http\Controllers\AbonneController@destroy')->name('destroyAbonne');
+Route::get('formulaire_Abonne', 'App\Http\Controllers\AbonneController@create')->name('createAbonne');
+Route::post('enregistrement_des_abonnes', 'App\Http\Controllers\AbonneController@store')->name('storeAbonne');
+
+
