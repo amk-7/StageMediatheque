71c3714d953dc75c97cf2572387ab4a768c79d52
Alhassan-Daiki <Alhassan.blog@gmail.com>
2022-08-23 11:38:11 +0000

    DocAudioVisuelController

--- source/mediatheque/app/Http/Controllers/DocumentAudioVisuelController.php
+++ source/mediatheque/app/Http/Controllers/DocumentAudioVisuelController.php
@@ -18,14 +18,14 @@ class DocumentAudioVisuelController extends Controller
      */
     public function index()
     {
-<<<<<<< HEAD
+
         //
         $documentAudioVisuels = DocumentAudioVisuel::all();
         return view('documentAudioVisuel.index')->with('documentAudioVisuels', $documentAudioVisuels);
-=======
+
         $documentAudioVisuel = DocumentAudioVisuel::all();
         return view("documentAudioVisuel.index", compact("documentAudioVisuel"))->paginate(25);
->>>>>>> dcbac1ccfdb22f9663055bb79b5286c3298d4a4e
+
     }
 
     /**
@@ -35,10 +35,10 @@ class DocumentAudioVisuelController extends Controller
      */
     public function create()
     {
-<<<<<<< HEAD
+
         //
         return view('documentAudioVisuel.create');
-=======
+
         $niveaus = [
             '1er degré', '2è degré', '3è degré', 'université'
         ];
@@ -67,7 +67,6 @@ class DocumentAudioVisuelController extends Controller
             'classification_dewey_centaines'=>$classification_dewey_centaines,
             'classification_dewey_dizaines'=>$classification_dewey_dizaines
         ]);
->>>>>>> dcbac1ccfdb22f9663055bb79b5286c3298d4a4e
     }
 
     /**
@@ -78,17 +77,16 @@ class DocumentAudioVisuelController extends Controller
      */
     public function store(Request $request)
     {
-<<<<<<< HEAD
+
         //
         $documentAudioVisuel = DocumentAudioVisuel::create([
             'genre' => $request->genre,
             'ISAN' => $request->ISAN
         ]);
         return redirect()->route('documentAudioVisuel.index');
-=======
+
         //--coder--
         return redirect()->route("documentAudioVisuel.index");
->>>>>>> dcbac1ccfdb22f9663055bb79b5286c3298d4a4e
     }
 
     /**
@@ -110,12 +108,11 @@ class DocumentAudioVisuelController extends Controller
      */
     public function edit(DocumentAudioVisuel $documentAudioVisuel)
     {
-<<<<<<< HEAD
         //
         return view('documentAudioVisuel.edit')->with('documentAudioVisuel', $documentAudioVisuel);
-=======
+
         return view("documentAudioVisuel.edite", compact("documentAudioVisuel"));
->>>>>>> dcbac1ccfdb22f9663055bb79b5286c3298d4a4e
+
     }
 
     /**
@@ -127,19 +124,19 @@ class DocumentAudioVisuelController extends Controller
      */
     public function update(Request $request, DocumentAudioVisuel $documentAudioVisuel)
     {
-<<<<<<< HEAD
+
         //
         $documentAudioVisuel->update(array([
             'genre' => $request['genre'],
             'ISAN' => $request['ISAN']
         ]));
         return redirect()->route('documentAudioVisuel.index');
-=======
+
         $ouvragePhysique = OuvragesPhysique::all()->where("id_ouvrage_physique", $documentAudioVisuel->id_ouvrage_physique);
         OuvragePhysiqueHelper::updateOuvrage($ouvragePhysique, $request["nombre_exemplaire"], $request["etat"], $request["disponibilite"]);
 
         return redirect()->route("documentAudioVisuel.index");
->>>>>>> dcbac1ccfdb22f9663055bb79b5286c3298d4a4e
+
     }
 
     /**
@@ -150,12 +147,12 @@ class DocumentAudioVisuelController extends Controller
      */
     public function destroy(DocumentAudioVisuel $documentAudioVisuel)
     {
-<<<<<<< HEAD
+
         //
         $documentAudioVisuel->delete();
         return redirect()->route('documentAudioVisuel.index');
-=======
+
         $documentAudioVisuel->delete();
->>>>>>> dcbac1ccfdb22f9663055bb79b5286c3298d4a4e
+
     }
 }
