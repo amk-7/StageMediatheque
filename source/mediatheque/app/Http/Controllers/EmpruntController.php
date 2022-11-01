c86786fd5376490e77dff04eec70b9ef7d1c4a86
Alhassan <Alhassan.blog@gmail.com>
2022-11-01 13:36:41 +0000

    Modif

--- source/mediatheque/app/Http/Controllers/EmpruntController.php
+++ source/mediatheque/app/Http/Controllers/EmpruntController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Emprunt;
+use App\Models\User;
+use App\Models\Abonne;
 use App\Models\LignesEmprunt;
 use App\Service\LignesEmpruntService;
 use Illuminate\Http\Request;
@@ -22,9 +24,21 @@ class EmpruntController extends Controller
     public function index()
     {
         //
-        $emprunts = Emprunt::all();
+        //$emprunts = Emprunt::all();
+        /*$utilisateur = User::all();
+        //dd($utilisateur.Emprunt::all()->first());
+        $emprunts = Emprunt::with('abonne')->get();*/
+        if ($_REQUEST['element'] ?? null) {
+            $emprunts = Emprunt::where('id_abonne', $_REQUEST['element'])->get();
+        } else {
+            $emprunts = Emprunt::all();
+        }
         //dd($emprunts);
-        return view('emprunt.index')->with('emprunts', $emprunts);
+        
+        return view('emprunt.index')->with([
+            'emprunts' => $emprunts,
+            "abonnes" => json_encode(AbonneService::getAbonnesWithAllAttribut()), 
+        ]);
     }
 
     /**
--- source/mediatheque/app/Service/AbonneService.php
+++ source/mediatheque/app/Service/AbonneService.php
@@ -30,6 +30,9 @@ class AbonneService
         return $abonnes;
     }
 
+
+   
+
 }
 
 
