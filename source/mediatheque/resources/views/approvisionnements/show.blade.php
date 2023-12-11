4ee34af20d24d8a752e39bc2f91c2883f97d3097
amk-7 <abdoulmalikkondi8@gmail.com>
2023-12-11 18:27:45 +0000

    pass to LARVEL 10

--- source/mediatheque/resources/views/approvisionnements/show.blade.php
+++
@@ -1,6 +0,0 @@
-@extends("layout.template.base")
-@section("content")
-    <main>
-        <h1>Approvisionement N°</h1>
-    </main>
-@endsection
--- source/mediatheque/vendor/mercuryseries/laravel-helpers/tests/HelpersTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class HelpersTest
-{
-    //TODO
-}
